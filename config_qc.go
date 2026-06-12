91e21088e9abcf03e7a093573299c88923b2d455
ndenny <nick@apicontext.com>
2026-06-11 18:08:38 -0700

    Add configuration files for production and QC environments; update main.go to reference them

---
+++ config_qc.go
@@ -0,0 +1,9 @@
+//go:build !prod
+
+package main
+
+var baseURL = "https://qc-client.apimetrics.io"
+var authURL = "https://qc-auth.apimetrics.io/authorize"
+var tokenURL = "https://qc-auth.apimetrics.io/oauth/token"
+var authAudience = "https://client.apimetrics.io"
+var clientID = "bj0yh0AjBMzfeOpffmCj5UP8FbmYDwcM"
